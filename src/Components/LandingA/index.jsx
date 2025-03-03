9f9b0ab44a08c9722042c5476b429e308492bff7
fmarcos38 <fmarcos_23@hotmail.com>
2025-03-03 11:19:21 -0300

    landings

--- src/Components/LandingA/index.jsx
+++ src/Components/LandingA/index.jsx
@@ -13,6 +13,16 @@ function LandingA() {
                 <p className='p2-texto-1'>Gam</p>
                 <p className='p3-texto-1'>La ciencia está de tu lado</p>
             </div>
+            <div className='cont-texto-2'>
+                <p className='p1-texto-2'>Tu belleza, al alcance de nuestras manos</p>
+            </div>
+            {/* flecha que señala hacia abajo compuesta por dos palitos blancos con movimiento hacia arriba y abjo */}
+            <div className='cont-flecha'>
+                <div className='flecha'>
+                    <div className='linea linea1'></div>
+                    <div className='linea linea2'></div>
+                </div>
+            </div>
         </div>
     )
 }
