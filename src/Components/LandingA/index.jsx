cb30a3bcafb7645b25469291f4aab03c6bdd7530
fmarcos38 <fmarcos_23@hotmail.com>
2025-06-05 17:35:58 -0300

    acomodando vistas

--- src/Components/LandingA/index.jsx
+++ src/Components/LandingA/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css'
 
 function LandingA() {
+
     return (
         <div className='cont-land-A'>
             <div className='cont-texto-1'>
